5ab21f3358c05407d5e3f74486ee97a7854db23b
mikhailUshakoff <mikhail.ushakov@yahoo.com>
2024-11-02 12:34:37 +0100

    Change tier provider to sp1

--- packages/protocol/contracts/layer1/devnet/DevnetTierProvider.sol
+++ packages/protocol/contracts/layer1/devnet/DevnetTierProvider.sol
@@ -15,13 +15,13 @@ contract DevnetTierProvider is TierProviderBase, ITierRouter {
     /// @inheritdoc ITierProvider
     function getTierIds() public pure override returns (uint16[] memory tiers_) {
         tiers_ = new uint16[](3);
-        tiers_[0] = LibTiers.TIER_OPTIMISTIC;
+        tiers_[0] = LibTiers.TIER_ZKVM_SP1;
         tiers_[1] = LibTiers.TIER_GUARDIAN_MINORITY;
         tiers_[2] = LibTiers.TIER_GUARDIAN;
     }
 
     /// @inheritdoc ITierProvider
     function getMinTier(address, uint256) public pure override returns (uint16) {
-        return LibTiers.TIER_OPTIMISTIC;
+        return LibTiers.TIER_ZKVM_SP1;
     }
 }
