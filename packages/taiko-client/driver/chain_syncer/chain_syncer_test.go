63e91c0d16311f15293f7b6cb69f2b946a3df3e0
Nurbakhyt Madibekov <jmadibekov@gmail.com>
2026-01-06 10:16:49 +0000

    Update chain syncer tests to use PendingNonceAt for nonce retrieval

--- packages/taiko-client/driver/chain_syncer/chain_syncer_test.go
+++ packages/taiko-client/driver/chain_syncer/chain_syncer_test.go
@@ -323,7 +323,8 @@ func (s *ChainSyncerTestSuite) TestShastaProposalWithMultipleBlocks() {
 	head1, err := s.RPCClient.L2.BlockByNumber(context.Background(), nil)
 	s.Nil(err)
 
-	nonce, err := s.RPCClient.L2.NonceAt(context.Background(), s.TestAddr, nil)
+	// Use PendingNonceAt to account for transactions that may be pending in the mempool
+	nonce, err := s.RPCClient.L2.PendingNonceAt(context.Background(), s.TestAddr)
 	s.Nil(err)
 
 	testTx1, err := testutils.AssembleAndSendTestTx(
@@ -373,7 +374,8 @@ func (s *ChainSyncerTestSuite) TestShastaProposalWithOneBlobAndMultipleBlocks() 
 	head1, err := s.RPCClient.L2.BlockByNumber(context.Background(), nil)
 	s.Nil(err)
 
-	nonce, err := s.RPCClient.L2.NonceAt(context.Background(), s.TestAddr, nil)
+	// Use PendingNonceAt to account for transactions that may be pending in the mempool
+	nonce, err := s.RPCClient.L2.PendingNonceAt(context.Background(), s.TestAddr)
 	s.Nil(err)
 
 	batches := 100
@@ -427,7 +429,8 @@ func (s *ChainSyncerTestSuite) TestShastaProposalWithTooMuchBlocks() {
 	head1, err := s.RPCClient.L2.BlockByNumber(context.Background(), nil)
 	s.Nil(err)
 
-	nonce, err := s.RPCClient.L2.NonceAt(context.Background(), s.TestAddr, nil)
+	// Use PendingNonceAt to account for transactions that may be pending in the mempool
+	nonce, err := s.RPCClient.L2.PendingNonceAt(context.Background(), s.TestAddr)
 	s.Nil(err)
 
 	txBatch := make([]types.Transactions, manifest.ProposalMaxBlocks+1)
@@ -468,7 +471,8 @@ func (s *ChainSyncerTestSuite) TestShastaProposalsWithInvalidForcedInclusion() {
 	head, err := s.RPCClient.L2.BlockByNumber(context.Background(), nil)
 	s.Nil(err)
 
-	nonce, err := s.RPCClient.L2.NonceAt(context.Background(), s.TestAddr, nil)
+	// Use PendingNonceAt to account for transactions that may be pending in the mempool
+	nonce, err := s.RPCClient.L2.PendingNonceAt(context.Background(), s.TestAddr)
 	s.Nil(err)
 
 	testTx, err := testutils.AssembleAndSendTestTx(
@@ -566,7 +570,8 @@ func (s *ChainSyncerTestSuite) TestShastaProposalsWithForcedInclusion() {
 	head, err := s.RPCClient.L2.BlockByNumber(context.Background(), nil)
 	s.Nil(err)
 
-	nonce, err := s.RPCClient.L2.NonceAt(context.Background(), s.TestAddr, nil)
+	// Use PendingNonceAt to account for transactions that may be pending in the mempool
+	nonce, err := s.RPCClient.L2.PendingNonceAt(context.Background(), s.TestAddr)
 	s.Nil(err)
 
 	testTx, err := testutils.AssembleAndSendTestTx(
